87d7205b4914629fc6fbf752d5ea52e4e87f653c
Chris Kim <chrisdohyunkim@gmail.com>
2025-09-08 16:20:10 +0000

    Format document

--- event.rb
+++ event.rb
@@ -1,13 +1,14 @@
 require "date"
+
 class Event
-    attr_accessor :item, :completed_date
+  attr_accessor :item, :completed_date
 
-    def initialize(item, completed_date)
-        @item = item
-        @completed_date = completed_date
-    end
+  def initialize(item, completed_date)
+    @item = item
+    @completed_date = completed_date
+  end
 
-    def to_s
-      "\"#{item}\" completed on #{completed_date}"
-    end
+  def to_s
+    "\"#{item}\" completed on #{completed_date}"
+  end
 end
--- list.rb
+++ list.rb
@@ -1,59 +1,62 @@
+Resolving dependencies...
 require_relative "event"
 
 class List
-    def initialize
-        @event = []
-    end
-
-    def run
-        loop do
-            puts "\n"
-            puts "Life is short! Start tracking your bucket list items!"
-            puts "1: Add a bucket list event you completed this year"
-            puts "2: List all your bucket list events completed during this year"
-            puts "3: Exit"
-            print "Choose an option: "
-            option = gets.chomp.to_i
-            puts "\n"
-            case option
-            when 1
-                add_event
-            when 2
-                list_events
-            when 3
-                puts "Keep adding to your bucket list! See you soon!"
-                break
-            else
-                puts "That's not an option. Please try again."
-            end
-        end
-    end
-
-    require "date"
-    def add_event
-        print "Enter which bucket list event you completed: "
-        item = gets.chomp
-        
-        completed_date = nil
-        loop do
-        print "Enter which date you completed the event (i.e. YYYY-MM-DD): "
-        input_date = gets.chomp
-
-        begin
-        completed_date = Date.parse(input_date)
-            break
-             rescue Error
-        puts "Please write the date in the correct format (YYYY-MM-DD)."
+  def initialize
+    @event = []
   end
-end
 
-        @event << Event.new(item, completed_date)
-        puts "Good job on another bucket list item completed!"
-end
-    def list_events
-        puts "These are the bucket list items you completed this year: "
-        @event.each do |event|
-            puts event
-        end
+  def run
+    loop do
+      puts "\n"
+      puts "Life is short! Start tracking your bucket list items!"
+      puts "1: Add a bucket list event you completed this year"
+      puts "2: List all your bucket list events completed during this year"
+      puts "3: Exit"
+      print "Choose an option: "
+      option = gets.chomp.to_i
+      puts "\n"
+      case option
+      when 1
+        add_event
+      when 2
+        list_events
+      when 3
+        puts "Keep adding to your bucket list! See you soon!"
+        break
+      else
+        puts "That's not an option. Please try again."
+      end
     end
+  end
+
+  require "date"
+
+  def add_event
+    print "Enter which bucket list event you completed: "
+    item = gets.chomp
+
+    completed_date = nil
+    loop do
+      print "Enter which date you completed the event (i.e. YYYY-MM-DD): "
+      input_date = gets.chomp
+
+      begin
+        completed_date = Date.parse(input_date)
+        break
+      rescue Error
+        puts "Please write the date in the correct format (YYYY-MM-DD)."
+      end
+    end
+
+    @event << Event.new(item, completed_date)
+    puts "Good job on another bucket list item completed!"
+  end
+
+  def list_events
+    puts "These are the bucket list items you completed this year: "
+    @event.each do |event|
+      puts event
+    end
+  end
 end
